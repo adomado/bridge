7912879b6776e7ebf7c5a2cf8da20f9000b3f121
makuchaku <makuchaku@adomado.com>
2011-08-16 15:11:13 +0530

    first commit

---
+++ bridge.rb
@@ -0,0 +1,40 @@
+require 'rubygems'
+require 'sinatra'
+require "uri"
+require 'net/http'
+#require 'net/https'
+require 'json'
+
+get '/' do
+  headers = params['h'] ? JSON.parse(params['h']) : {}
+  url = URI.parse(params['u'])
+
+  if params['m'] == 'get'
+    request = Net::HTTP::Get.new(url.path)
+  elsif params['m'] == 'post'
+    request = Net::HTTP::Post.new(url.path)
+  elsif params['m'] == 'put'
+    request = Net::HTTP::Put.new(url.path)
+  elsif params['m'] == 'delete'
+    request = Net::HTTP::Delete.new(url.path)
+  elsif params['m'] == 'head'
+    request = Net::HTTP::Head.new(url.path)
+  else
+    request = Net::HTTP::Get.new(url.path)
+  end
+
+  headers.each do |key, value|
+    request.add_field(key, value)
+  end
+
+  request.body = params['b'] if params['b']
+
+  http = Net::HTTP.new(url.host, url.port)
+  http.use_ssl = false
+  response = http.start do |http|
+    http.request(request)
+  end
+
+  arg = "{ status: #{response.code}, headers: [#{response.each_name { }.to_json}], body: '#{response.body}' }"
+  json = "#{params['jsonp']}(#{arg});"
+end
---
+++ config.ru
@@ -0,0 +1,6 @@
+require 'rubygems'
+require 'sinatra'
+require 'bridge.rb'
+
+run Sinatra::Application
+
